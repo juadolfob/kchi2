63c9af6ba29d5ef3c94d4d61a62a6940e13bcd2e
Pérez <rogelio-antonio.perez@atos.net>
2021-11-24 13:46:08 -0600

    Creation project

---
+++ src/main/java/controllers/StudentController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import models.Student;
+import templates.StudentTemplate;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@RequestMapping("/index")  
+    public String index(Model model)  
+    {  
+		List<Student> list = new StudentTemplate().getAllStudents();
+		model.addAttribute("students", list);
+		return "/student/index";
+    }  
+}
---
+++ src/main/java/models/Student.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Student {
+	int sid;
+	String sname;
+	String scity;
+	int sscore;
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getScity() {
+		return scity;
+	}
+	public void setScity(String scity) {
+		this.scity = scity;
+	}
+	public int getSscore() {
+		return sscore;
+	}
+	public void setSscore(int sscore) {
+		this.sscore = sscore;
+	}
+}

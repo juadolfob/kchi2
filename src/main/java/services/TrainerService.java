ba02fefbd0ccc7caf752670beb13d1d17730c659
Alvarado <ricardo-emmanuel.alvarado@atos.net>
2021-12-01 14:59:25 -0700

    GetApproveSlot improved

--- src/main/java/services/TrainerService.java
+++ src/main/java/services/TrainerService.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import models.TrainingProposals;
+import models.LDMemberData;
 import models.TrainingRequirementMaster;
 
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import mappers.TrainingProposalsMapper;
+import mappers.LDMembermapper;
 
 public class TrainerService {
 	JdbcTemplate template;
@@ -38,11 +40,11 @@ public class TrainerService {
 	}
 	
 	public TrainingProposals GetApproveSlot(String executionID){
-		TrainingProposals trainingApprove = new TrainingProposals();
-		
+		TrainingProposals trainingApprove;
 		Object[] params = new Object[]{executionID};
 		trainingApprove = this.template.queryForObject("select * from TrainingProposals where ExecutionID = ?", params, new TrainingProposalsMapper());
-		
+		LDMemberData member = this.template.queryForObject("select * from TrainingProposals where ExecutionID = ?", params, new LDMembermapper());
+		trainingApprove.setMemberID(member);
 		return trainingApprove;
 	}
 	
